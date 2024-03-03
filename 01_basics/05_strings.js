2a4fb51bdedc349fa14f889fadbe21072fcf6b1f
SmitBalaramMore <157155076+SmitBalaramMore@users.noreply.github.com>
2024-03-03 20:05:03 +0000

    string methods

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,25 @@
+let name="smit"
+let age="21"
+
+console.log(`my name is ${name} and my age is ${age}`)
+
+
+let gamename= new String("helloworldfrommore")
+console.log(gamename);
+
+console.log(gamename.split(''))
+console.log(gamename[1])
+console.log(gamename.length);
+console.log(gamename.toUpperCase(2));
+console.log(gamename.replace("l","s"));
+console.log(gamename.charAt(2));
+console.log(gamename.indexOf("d"));
+
+
+
+//const flashgame= gamename.substring(0,5)
+//console.log(flashgame);
+
+//slice can be used for negative indexing
+ const anotherstringe = gamename.slice(5,-3)
+ console.log(anotherstringe);
