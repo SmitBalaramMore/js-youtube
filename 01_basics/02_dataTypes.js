ad8547b1a4d37b6225ae45d53e8ef39b37050134
SmitBalaramMore <157155076+SmitBalaramMore@users.noreply.github.com>
2024-03-03 11:50:47 +0000

    types of data explained

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict";//treat all js code as a newer version
+
+// alert(3+3)// we are using node js, not a browser
+
+
+let name="smit"
+let age= 20
+let isLogged = true
+let temp= null
+
+
+
+//dataTypes
+//primitive dataTypes:
+
+// number =>
+// bigInt => used for large numbers mostly used in like stockmarkets
+// stringe => "i am a stringe"
+// boolean => True/False (yes/no)
+// null => stand alone value (its a object) its not a undefined it  means empty
+// undefined => means value is not defined or assigned 
+// symbol => its used to uniquness(Symbols are often used to add unique property 
+// keys to an object that won't collide with keys any other code might add to the object)
+
+//non-primitive data-types
+//object =>
+//array => With the help of an array, we can store more than one element under a single name.
