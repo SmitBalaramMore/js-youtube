0af9b4feeb74cf6c9436137484323d759d81e5f4
SmitBalaramMore <157155076+SmitBalaramMore@users.noreply.github.com>
2024-03-03 13:54:42 +0000

    prefix and postfix

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -37,15 +37,27 @@ console.log(1*2)
 console.log(2/2)
 console.log(3%2)
 console.log(2**3)
-
 */
 
+
+/*
 //this is not a good method to write a code 
 console.log("1" + 2) // output will be 12
 console.log(2+2+"3")// output will be 43
 
-// so we give parentheses to prioritize
 
+
+// so we give parentheses to prioritize
 console.log((2+1)+4)
 console.log((1+1)+"4")//the output will be 24 
+*/
+
+
+/*prefix => ++1
+postfix => 1++
+
+
+let count=100
+let y = ++count;
+console.log(count,y)*/
 
