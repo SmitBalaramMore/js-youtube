cb7a41343d742096f16703415582e8b236dfc453
SmitBalaramMore <157155076+SmitBalaramMore@users.noreply.github.com>
2024-03-03 13:12:12 +0000

    operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -22,3 +22,30 @@ console.log( convertpanme);
 // 1 => true , 0=> false
 // "" => false
 //"smitmore" => True
+
+
+//  *********************** Operations ************************************ //
+/*
+let value=5
+let negvalue= -value// this will convert positive number into negative
+console.log(negvalue);
+
+
+console.log(1+2)
+console.log(4-2)
+console.log(1*2)
+console.log(2/2)
+console.log(3%2)
+console.log(2**3)
+
+*/
+
+//this is not a good method to write a code 
+console.log("1" + 2) // output will be 12
+console.log(2+2+"3")// output will be 43
+
+// so we give parentheses to prioritize
+
+console.log((2+1)+4)
+console.log((1+1)+"4")//the output will be 24 
+
