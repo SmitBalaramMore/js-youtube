570d60ba9458cd38bbfa31fb65028ed13c7bd4ae
SmitBalaramMore <157155076+SmitBalaramMore@users.noreply.github.com>
2024-03-03 12:24:03 +0000

    Conversion of dataTypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,24 @@
+//let age= "21"
+//console.log(typeof age)//this will show in age in stringe
+
+// now let's convert this into number
+//let convert=Number(age);
+//console.log(typeof convert)
+
+// "33" => 33
+// "33abc" => NaN
+// true => , false => 0 
+
+
+
+/*
+let pname=""
+console.log(typeof pname);
+
+let convertpanme = Boolean(pname)
+console.log( convertpanme); 
+*/
+
+// 1 => true , 0=> false
+// "" => false
+//"smitmore" => True
