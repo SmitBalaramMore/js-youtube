061b5db18db14b3cbde55cbad929b1da071a9efa
SmitBalaramMore <157155076+SmitBalaramMore@users.noreply.github.com>
2024-03-04 16:42:44 +0000

    about date object

---
+++ 01_basics/07_dateANDtime.js
@@ -0,0 +1,9 @@
+let date= new Date()//date is object
+console.log(date)
+console.log(date.getDay())
+console.log(date.getMonth()+1)// remember date month start with 0
+console.log(date.toString())
+
+
+let newdate = new Date(2024,3,4)
+console.log(newdate.toDateString())
