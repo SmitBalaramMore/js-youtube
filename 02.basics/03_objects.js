ca3d28c68ec972cd32d146f918938507a54867c1
SMITMORE <157155076+SmitBalaramMore@users.noreply.github.com>
2024-03-07 15:08:07 +0000

    obj

--- 02.basics/03_objects.js
+++ 02.basics/03_objects.js
@@ -1,14 +1,27 @@
 //how to declare symbols
-const mysym=Symbol("smit")
+// const mysym=Symbol("smit")
 
 
-console.log(mysym)
+// console.log(mysym)
 
-let obj={
-    [mysym]:"smit",
-    age:"21",
-    gender:",male"
-}
-console.log(typeof mysym)
+// let obj={
+//     [mysym]:"smit",
+//     age:"21",
+//     gender:",male"
+// }
+// console.log(typeof mysym)
+
+const objnew={}
+
+
+objnew.name="smit"
+
+
+
+console.log(objnew);
+
+
+
+
 
 
