861c725c6759441630b77036f3726d222f189c9c
SMITMORE <157155076+SmitBalaramMore@users.noreply.github.com>
2024-03-11 08:30:17 +0000

    added some methods and notes

--- 02.basics/02_arrays.js
+++ 02.basics/02_arrays.js
@@ -9,5 +9,24 @@ const dc_heros = ["superman", "flash", "batman"]
 
 //now we will concat the array in single array not in nested array using concat
 //it's need new variable to hold new concat array
- const array1=marvel_heros.concat(dc_heros)
- console.log(array1)
+ //const array1=marvel_heros.concat(dc_heros)
+// console.log(array1)
+
+//const all_heros=[...marvel_heros,...dc_heros];//this a spread operator here we are using bcz we can add many arrays in one 
+//console.log(typeof all_heros);
+
+//const everyhero=[1,2,3,4,[1,3,4,4,6,[4,5,6,6]]]
+//const allhero=everyhero.flat(4);//this will add array inside array in one and you have give lenth of array you want in one
+//console.log(allhero);
+
+console.log(Array.isArray([1,2,3,4]))//check the value if it is array
+console.log(Array.from("smitmore"))//converts into array
+
+
+
+let score1=100
+let score2=200
+let score3=300
+
+console.log(Array.of(score1,score2,score3))
+
