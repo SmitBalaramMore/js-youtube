5f89944f1d702dd45b580418053cbb53ccb6d1d3
SMITMORE <157155076+SmitBalaramMore@users.noreply.github.com>
2024-03-11 17:32:08 +0000

    scopes

---
+++ 03.basics/02_scopes.js
@@ -0,0 +1,23 @@
+//scopes 
+//let and const are cannot be used outside the {} var can be used so it can create problem
+//when we give variable inside {} is block scope 
+//when we give outside its a global scope 
+
+//block scope:
+
+if(true){
+    let a=5
+    const b=6
+
+}
+
+//global scope 
+let a=10
+const b=15
+
+if(ture){
+    a+b
+}
+
+
+
