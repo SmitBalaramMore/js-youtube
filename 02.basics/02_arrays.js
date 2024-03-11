3d9e52674aa4454ee5e9800655152aab6e556aa8
SMITMORE <157155076+SmitBalaramMore@users.noreply.github.com>
2024-03-11 07:24:03 +0000

    notes

--- 02.basics/02_arrays.js
+++ 02.basics/02_arrays.js
@@ -1,31 +1,13 @@
 const marvel_heros = ["thor", "Ironman", "spiderman"]
 const dc_heros = ["superman", "flash", "batman"]
 
-// marvel_heros.push(dc_heros)
+//marvel_heros.push(dc_heros)//this will add the 2nd array in another you can add any data
+//types in array
 
-// console.log(marvel_heros);
-// console.log(marvel_heros[3][1]);
-
-// const allHeros = marvel_heros.concat(dc_heros)
-// console.log(allHeros);
-
-const all_new_heros = [...marvel_heros, ...dc_heros]
-
-// console.log(all_new_heros);
-
-const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
-
-const real_another_array = another_array.flat(Infinity)
-console.log(real_another_array);
+//console.log(marvel_heros[3][2])//so it is getting array in array's value
 
 
-
-console.log(Array.isArray("Hitesh"))
-console.log(Array.from("Hitesh"))
-console.log(Array.from({name: "hitesh"})) // interesting
-
-let score1 = 100
-let score2 = 200
-let score3 = 300
-
-console.log(Array.of(score1, score2, score3));
+//now we will concat the array in single array not in nested array using concat
+//it's need new variable to hold new concat array
+ const array1=marvel_heros.concat(dc_heros)
+ console.log(array1)
